0c3feb8b3a3d4da3cc5f2e83ca074e1c9854ca61
ujamaar <narendra.r.joshi@gmail.com>
2014-12-11 11:09:00 -0500

    added script for place cells

--- heatmap_place_cell.py
+++ heatmap_place_cell.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
 
-event_data = np.loadtxt('/Users/njoshi/Desktop/test_data/behavior_and_imaging_combined.csv', dtype='float', delimiter=',')
+event_data = np.loadtxt('/Users/njoshi/Desktop/test_data/sample_events_for_place_cell.csv', dtype='float', delimiter=',')
 #event_data = event_data[0:22,0:5]
 #to plot time as x-axis, transpose the whole array
 event_data = event_data.transpose()
@@ -11,7 +11,6 @@ print "Events array size is: (%d ,%d)" %(event_data.shape[0], event_data.shape[1
 
 odor = event_data[2,:]
 lap_count = event_data[6,:]
-#environment = event_data[7,:]
 total_laps = max(lap_count) + 1
 print 'There are %d laps in this trial' %total_laps
 #print odor[0:15]
@@ -29,11 +28,16 @@ print 'There are %d cells to be plotted' %total_number_of_cells
 #print a portion of the event data just to check
 #print event_data[0:10,0:10]
 
+
+
 ###############################################################################
 #if there are too many time points, condense the data points to keep the array size within a sane range
 #lets say the size of the figure will be adjusted to always have 100 time points
 number_of_odors = 4
 event_data_for_plotting = np.empty((event_data.shape[0],total_laps*number_of_odors)) 
+
+
+
 
 for row in range(0,event_data.shape[0]):
     current_lap = 0
@@ -85,55 +89,56 @@ for row in range(0,event_data_averaged_over_laps.shape[0]):
                 laps_with_events = laps_with_events + 1.00
         event_data_averaged_over_laps[row][column] = (laps_with_events / total_lap_count)*100.00
 
-print 'Sample of summed event values'     
-print event_data_averaged_over_laps[0:10,0:10]
+
 
 ###############################################################################
 ###################### now we plot some good nice heatmaps ####################
 ###############################################################################
 
-def generate_plots(data_array):
+def generate_plots(data_array,sorted_column):
 
     figs = []
+    
+    #you can choose to limit how many cells get plotted in one graph
+    cells_in_one_plot = total_number_of_cells
+    last_cell_index = 0
+    
+    keep_plotting_figures = True
+    
+    while(keep_plotting_figures):    
+        fig = plt.figure()
+        fig.suptitle('Plot of %d sorted cells'%total_number_of_cells, fontsize=12)
+        ax1 = plt.subplot2grid((4,4), (0,0), rowspan=4)
+#        ax2 = plt.subplot2grid((3,4), (0,1), rowspan=3)
+#        ax3 = plt.subplot2grid((3,4), (0,2), rowspan=3)
+#        ax4 = plt.subplot2grid((3,4), (0,3), rowspan=3)
         
-    fig = plt.figure()
-    fig.suptitle('Plot of %d sorted cells'%total_number_of_cells, fontsize=12)
-    #fig.set_size_inches(10,2) 
-    ax1 = plt.subplot2grid((3,4), (0,0), rowspan=3)
-    ax2 = plt.subplot2grid((3,4), (0,1), rowspan=3, sharex=ax1, sharey=ax1)
-    ax3 = plt.subplot2grid((3,4), (0,2), rowspan=3, sharex=ax1, sharey=ax1)
-    ax4 = plt.subplot2grid((3,4), (0,3), rowspan=3, sharex=ax1, sharey=ax1)
-    
-    #sort the cells according to their response to an odor     
-    plot_data1 = data_array[np.argsort(data_array[:,0],kind='quicksort')]
-    plot_data2 = data_array[np.argsort(data_array[:,1],kind='quicksort')]
-    plot_data3 = data_array[np.argsort(data_array[:,2],kind='quicksort')]
-    plot_data4 = data_array[np.argsort(data_array[:,3],kind='quicksort')]
-    
-    #now make the actual plot using pcolor
-    ax1.pcolor(plot_data1, cmap=plt.cm.Blues)
-    ax2.pcolor(plot_data2, cmap=plt.cm.Reds)
-    ax3.pcolor(plot_data3, cmap=plt.cm.Greens)
-    ax4.pcolor(plot_data4, cmap=plt.cm.Reds) 
-    
-    #the x and y axis labels of ax1 are shared by all other subplots
-    ax1.xaxis.tick_bottom()
-    ax1.set_xlabel('Odor')
-    ax1.set_ylabel('Cell#')
-    row_labels = list(' 1 2 3 4')
-    ax1.set_xticklabels(row_labels, minor=False)
+        
+        if(last_cell_index + cells_in_one_plot >= total_number_of_cells):
+            cells_in_one_plot =  total_number_of_cells - last_cell_index
+            keep_plotting_figures = False
+         
+        plot_data = data_array[last_cell_index:last_cell_index + cells_in_one_plot,:]
+        last_cell_index = last_cell_index + cells_in_one_plot
+        
+        #now make the actual plot using pcolor
+        event_heatmap = ax1.pcolor(plot_data, cmap=plt.cm.Blues)
+        
+        ax1.xaxis.tick_bottom()
 
-    plt.ylim (0,total_number_of_cells)
-    
-    #suppress the y labels of all subplots except ax1
-    yticklabels = ax2.get_yticklabels()+ax3.get_yticklabels()+ax4.get_yticklabels()  
-    plt.setp(yticklabels, visible=False)
-    
-    #can be commented out to stop showing all plots in the console
-    plt.show()
-    
-    figs.append(fig)
-    plt.close()
+        ax1.set_xlabel('Odor')
+        ax1.set_ylabel('Cell#')
+        #plt.xlim(0,plot_data.shape[1])
+        plt.ylim (0 , cells_in_one_plot)
+        
+        row_labels = list(' 1 2 3 4')
+        ax1.set_xticklabels(row_labels, minor=False)
+        
+        #can be commented out to stop showing all plots in the console
+        plt.show()
+        
+        figs.append(fig)
+        plt.close()
         
     if len(figs) > 0:
         pdf_name = '/Users/njoshi/Desktop/test_data/test_plot.pdf'
@@ -143,6 +148,14 @@ def generate_plots(data_array):
         pp.close() 
 
 ###############################################################################
-###################### just a final touch here ################################
 ###############################################################################
-generate_plots(event_data_averaged_over_laps)
+###############################################################################
+
+#sort the cells according to size of summed events along the desired odor column
+sort_column = 0
+now_plot_this = event_data_averaged_over_laps[np.argsort(event_data_averaged_over_laps[:,sort_column],kind='quicksort')]
+
+print 'Sample of summed event values'     
+print now_plot_this[0:10,0:10]
+
+generate_plots(now_plot_this,sort_column)
