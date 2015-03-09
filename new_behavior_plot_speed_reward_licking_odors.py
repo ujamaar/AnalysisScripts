30c1f2f30b4a4329e04589e0f9a316118239e52a
ujamaar <narendra.r.joshi@gmail.com>
2015-03-09 18:12:54 -0400

    new scripts for heatmaps of randomized environments and time sequences

---
+++ new_behavior_plot_speed_reward_licking_odors.py
@@ -0,0 +1,310 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 30 18:52:38 2014
+
+@author: njoshi
+"""
+#something for inspiration: http://matplotlib.org/examples/pylab_examples/multiple_yaxis_with_spines.html
+import numpy
+import os
+from pylab import *
+from matplotlib.backends.backend_pdf import PdfPages
+import re
+
+directory_path ='/Users/njoshi/Desktop/events_test'
+#directory_path ='C:/cygwin/home/axel/RotaryDisc_CABA/Recordings/nwp68/nwp68_2014_11_05'
+#\\losonczy-server\walter\Virtual_Odor\behavior_data\wfnjC3\wfnjC3_2014_10_06
+
+def generate_graph(filename,track_length):
+    
+    behavior_data = np.loadtxt(filename, dtype='int', delimiter=',',skiprows=2)
+
+    #column 0 = frame number
+    #column 1 = time
+    #column 2 = odor
+    #column 3 = licks
+    #column 4 = rewards
+    #column 5 = initial drop
+    #column 6 = reward window
+    #column 7 = distance
+    #column 8 = total distance
+    #column 9 = lap
+    #column 10 = environment
+
+###################################################################      
+#Calculate speed(measured as time taken to travel each 50mm stretch of the track):
+###################################################################
+
+#    #calculate speed per 50mm stretch of the track########################################
+#
+#    s1 = 0.00
+#    s2 = 0.00
+#    t1 = 0.00
+#    t2 = 0.00
+##    last_line_fill = 0
+#    speed = numpy.zeros(len(behavior_data),dtype='float')
+#   
+#    current_speed = 0.00
+#    #current_lick_rate = 0.0    
+#    
+#    for line in range(0,len(behavior_data)):
+#        #read current distance and time
+#        s2 = behavior_data[line][7]
+#        #sanity_check = raw_behavior[line-1][6]
+#        #if there is no change in time, the speed remains unchanged
+#        #speed is calculated for every 500 ms window in this case
+#        if (s2 >= ((int(s1/100.00) + 1)*100.00)):# and (abs(s2-sanity_check) < 20.00)):
+#            t2 = behavior_data[line-1][1]
+#            s2 = behavior_data[line-1][7]
+#            current_speed = (s2 - s1)*100/(t2 - t1) #speed is in cm/s
+#            
+#            speed[line-1] = current_speed
+#            
+#            t1 = behavior_data[line][1]
+#            s1 = behavior_data[line][7]
+#            
+##            for blank_line in range(last_line_fill,line):
+##                speed[blank_line] = current_speed
+##            last_line_fill = line
+#        #at the end of reward region, when the distance measurement resets to zero
+#        elif(s2 < s1 and s1-s2 > 100.00):
+#            t2 = behavior_data[line-1][1]
+#            s2 = behavior_data[line-1][7]          
+#            current_speed = (s2 - s1)*100/(t2 - t1) #speed is in cm/s
+#
+#            speed[line-1] = current_speed
+##            for blank_line in range(last_line_fill,line):
+##                speed[blank_line] = current_speed
+##            last_line_fill = line            
+#            t1 = behavior_data[line][1]
+#            s1 = behavior_data[line][7]
+#        #at the end of the recording
+#        elif(line == len(behavior_data)-1):
+#            t2 = behavior_data[line][1]
+#            s2 = behavior_data[line][7]          
+#            current_speed = (s2 - s1)*100/(t2 - t1) #speed is in cm/s
+#
+#            speed[line-1] = current_speed
+##            for blank_line in range(last_line_fill,line+1):
+##                speed[blank_line] = current_speed          
+
+###################################################################      
+#Calculate speed(measured as distance traveled every 500ms):
+###################################################################
+
+    #calculate speed every 500ms
+    s1 = 0.00    
+    s2 = 0.00 #float(behavior_data[0,7])
+    t1 = 0.00 
+    t2 = 0.00 #float(behavior_data[0,1])
+    
+    speed = numpy.zeros(behavior_data.shape[0],dtype='float')
+
+    for line in range(0,behavior_data.shape[0]):
+        #read current distance
+        t2 = float(behavior_data[line][1])
+        s2 = float(behavior_data[line][7])
+        
+        #if there is no change in time, the speed remains unchanged
+        #speed is calculated for every 2000 ms window in this case
+        if (t2 - t1 > 500 and abs(s2-s1) < 2000):
+            t2 = behavior_data[line-1][1]
+            s2 = behavior_data[line-1][7]
+            speed[line-1] = (s2 - s1)*100.00/(t2 - t1) #speed is in cm/s
+            t1 = behavior_data[line][1]
+            s1 = behavior_data[line][7]
+        #at the end of reward region, when the distance measurement resets to zero
+        elif(abs(s2-s1) > 2000): #to reset values when a new track starts
+            t2 = behavior_data[line-1][1]
+            s2 = behavior_data[line-1][7]
+            speed[line-1] = (s2 - s1)*100.00/(t2 - t1) #speed is in cm/s
+            speed[line] = (s2 - s1)*100.00/(t2 - t1) #speed is in cm/s
+            t1 = behavior_data[line][1]
+            s1 = behavior_data[line][7]
+        #at the end of the recording
+        elif(line == len(behavior_data)-1): 
+            speed[line] = (s2 - s1)*100.00/(t2 - t1) #speed is in cm/s      
+                
+
+###################################################################      
+#for each lap, generate the data points to be plotted:
+###################################################################
+    
+    figs = []
+    
+    #get the total number of laps in this recording
+    lap_count = np.int(max(behavior_data[:,9])) + 1
+    print 'Total number of laps: %d' %lap_count
+
+    color_options = ['y','b','g','r','c','m']   
+    
+    #now generate the plots    
+    for current_lap in range(0,lap_count):
+        print current_lap+1
+
+        x1 = []
+        y1 = []
+        
+        x2 = []
+        y2 = []
+        
+        x3 = []
+        y3 = []
+        
+        x4 = []
+        y4 = []
+        
+        x5 = []
+        y5 = []
+        
+        last_row_in_current_lap = 0
+        for row in range(1,len(behavior_data)):
+            if (behavior_data[row][9] == current_lap):
+                #speed vs. distance
+                if speed[row] != 0:
+                    x1.append(behavior_data[row][7])
+                    y1.append(speed[row])
+                
+                #rewards vs. distance
+                if behavior_data[row][4] > behavior_data[row-1][4]:
+                    x2.append(behavior_data[row][7])
+                    y2.append(behavior_data[row][4])
+                        
+                #licsk vs. distance
+                if behavior_data[row][3] > behavior_data[row-1][3]:
+                    x3.append(behavior_data[row][7])
+                    y3.append(behavior_data[row][3])
+
+                #odor valves vs. distance      
+                if behavior_data[row][2] != behavior_data[row-1][2]:
+                    #x4.append(behavior_data[row-1][7])
+                    #y4.append(behavior_data[row-1][2])  
+                    x4.append(behavior_data[row][7])
+                    y4.append(behavior_data[row][2]) 
+
+                #initial drop vs. distance    
+                if behavior_data[row][5] > behavior_data[row-1][5]:
+                    x5.append(behavior_data[row][7])
+                    y5.append(behavior_data[row][5] * 100)
+                
+                last_row_in_current_lap = row
+        
+        
+###################################################################      
+#now plot the data in a figure (with 4 subplots in this case):
+###################################################################
+
+        fig = figure()
+        
+        subplots_adjust(hspace=0)
+        figtext(0.45,0.96, "Lap: %s"%(current_lap+1), fontsize='large', color='k', ha ='left') 
+
+        #print x1
+        #print y1                       
+        ax1 = subplot(311)
+        ax1.plot(x1,y1,'b-', linewidth = 0.01)
+        for odor in range(0,len(x4)/2):
+            #shade the odor region in a light color
+            ax1.axvspan(x4[odor*2], x4[odor*2+1], facecolor=color_options[y4[odor*2]], alpha=0.1,linewidth=0.1)
+            
+            #label the odor numbers just above the plot in red color          
+            if(x4[odor*2] % (track_length/4 - 100) < 200):
+                figtext(0.3,0.91, "%s"%y4[odor*2], fontsize='large', color='r', ha ='left')
+            elif(x4[odor*2] % (track_length/2 - 100) < 200):
+                figtext(0.46,0.91, "%s"%y4[odor*2], fontsize='large', color='r', ha ='left')
+            elif(x4[odor*2] % ((3*track_length)/4 - 100) < 200):
+                figtext(0.62,0.91, "%s"%y4[odor*2], fontsize='large', color='r', ha ='left')  
+
+        if len(x5) > 0:
+            ax1.vlines(x5,0,y5, color = 'r', linewidth = 0.01) #to mark the presentation of initial drop
+        else:
+            ax1.vlines(track_length,0,100, color = 'k', linestyle='dotted', linewidth = 0.01) #else just draw a black line to mark start of reward window
+#        if(behavior_data[last_row_in_current_lap][7] > 4000):
+        ax1.vlines(behavior_data[last_row_in_current_lap][7],0,100, color = 'k', linestyle='solid', linewidth = 0.05) #to mark end of the track
+        ax1.set_ylabel('Speed(cm/s)', fontsize='xx-small')
+        ax1.tick_params(axis='both', which='major', labelsize=5)
+        #ax1.set_title(filename, fontsize=10)
+        xlim(0,track_length + track_length/4) 
+        ylim(0,100)
+
+
+        ax2 = subplot(312, sharex=ax1)
+        ax2.vlines(x2,0,y2, color='r', linewidth = 0.01)
+        for odor in range(0,len(x4)/2):
+            ax2.axvspan(x4[odor*2], x4[odor*2+1], facecolor=color_options[y4[odor*2]], alpha=0.1,linewidth=0.1)
+        ax2.set_ylabel('Rewards', fontsize='xx-small')
+        ylim(0,1)      
+        
+        ax3 = subplot(313, sharex=ax1)
+        ax3.vlines(x3,0,y3,color = 'g', linewidth = 0.01)
+        #ax3.vlines(x6,0,y6, color ='r', linewidth = 0.01)
+        for odor in range(0,len(x4)/2):
+            ax3.axvspan(x4[odor*2], x4[odor*2+1], facecolor=color_options[y4[odor*2]], alpha=0.1,linewidth=0.1)
+        ax3.set_ylabel('Licks', fontsize='xx-small')
+        ax3.set_xlabel('Distance along the virtual track(mm)', fontsize='small')
+        xlim(0,track_length + track_length/4)
+        ylim(0,1)
+        
+#        ax4 = subplot(414, sharex=ax1)
+#        #ax4.plot(x4,y4,'r-', linewidth = 0.01)
+#        for odor in range(0,len(x4)/2):
+#            ax4.axvspan(x4[odor*2], x4[odor*2+1], facecolor=color_options[y4[odor*2]], alpha=0.5,linewidth=0.1)
+#            if(x4[odor*2]%900 < 200):
+#                figtext(0.3,0.2, "%s"%y4[odor*2], fontsize='large', color='k', ha ='left')
+#            elif(x4[odor*2]%1900 < 200):
+#                figtext(0.46,0.2, "%s"%y4[odor*2], fontsize='large', color='k', ha ='left')
+#            elif(x4[odor*2]%2900 < 200):
+#                figtext(0.62,0.2, "%s"%y4[odor*2], fontsize='large', color='k', ha ='left')                
+#        ax4.vlines(x5,0,y5, color = 'b', linewidth = 0.01)
+#        ax4.set_ylabel('Odor', fontsize='xx-small')
+#        ax4.set_xlabel('Distance along the virtual track(mm)', fontsize='xx-small')
+#        xlim(0,5000)
+#        ylim(0,4)
+  
+        
+        xticklabels = ax1.get_xticklabels()+ax2.get_xticklabels() #+ax3.get_xticklabels()  
+        setp(xticklabels, visible=False)
+
+        yticklabels = ax2.get_yticklabels()+ax3.get_yticklabels() #+ax4.get_yticklabels()  
+        setp(yticklabels, visible=False)
+        
+        #to set line width of the axes
+        for axis in ['top','bottom','left','right']:
+          ax1.spines[axis].set_linewidth(0.01)  
+          ax2.spines[axis].set_linewidth(0.01) 
+          ax3.spines[axis].set_linewidth(0.01) 
+          #ax4.spines[axis].set_linewidth(0.01) 
+        
+        figs.append(fig)
+        
+        #plt.annotate()
+        plt.close()
+
+###################################################################      
+#Save all plots in a pdf file in the same folder as the source data file:
+###################################################################
+
+    pdf_name = filename.replace(".csv","_behavior_plots.pdf")
+    pp = PdfPages(pdf_name)    
+    for fig in figs:
+        pp.savefig(fig)#, dpi=1000, 
+    pp.close()
+
+#to make sure that the files are processed in the proper order
+def natural_key(string_):
+    """See http://www.codinghorror.com/blog/archives/001018.html"""
+    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+
+def main():
+    file_names = [os.path.join(directory_path, f)
+        for dirpath, dirnames, files in os.walk(directory_path)
+        for f in files if f.endswith('.csv')]
+    file_names.sort(key=natural_key)
+    
+    #now generate a pdf file with all the plots (one plot for each file)
+    for filez in file_names:
+            print "File to be processed: " + filez
+            track_length = 4000
+            generate_graph(filez,track_length)
+
+main()
---
+++ new_combine_behavior_and_imaging.py
@@ -0,0 +1,299 @@
+import os
+import numpy
+import re
+
+#this script is for selecting valid cells from events file and then combining the behavior data with event data
+
+#for PC, the format is something like: directory_path ='C:/Users/axel/Desktop/test_data'
+directory_path ='/Users/njoshi/Desktop/events_test'
+
+def extract_details_per_frame (events_file, valid_cells_file, raw_behavior_file, missing_frame_file):
+    #fast_output_array = numpy.loadtxt(complete_file, dtype='int', comments='#', delimiter=',', converters=None, skiprows=2, usecols=(0,1,3,5,6,12,15), unpack=False, ndmin=0) 
+    
+    raw_behavior = numpy.loadtxt(raw_behavior_file, dtype='int', comments='#', delimiter=',',skiprows=2)
+    
+#    #calculate speed per 50mm stretch of the track########################################
+#    s1 = 0.00
+#    s2 = 0.00
+#    t1 = 0.00
+#    t2 = 0.00
+#    last_line_fill = 0
+#    speed = numpy.zeros(len(raw_behavior),dtype='float')
+#
+#    
+#    current_speed = 0.00
+#    #current_lick_rate = 0.0    
+#    
+#    for line in range(0,len(raw_behavior)):
+#        #read current distance and time
+#        s2 = raw_behavior[line][7]
+#        #sanity_check = raw_behavior[line-1][6]
+#        #if there is no change in time, the speed remains unchanged
+#        #speed is calculated for every 500 ms window in this case
+#        if (s2 >= ((int(s1/50.00) + 1)*50.00)):# and (abs(s2-sanity_check) < 20.00)):
+#            t2 = raw_behavior[line-1][1]
+#            s2 = raw_behavior[line-1][7]
+#            current_speed = ((s2 - s1)*1000.00)/(t2 - t1) #speed is in mm/s
+#
+#            t1 = raw_behavior[line][1]
+#            s1 = raw_behavior[line][7]
+#            
+#            for blank_line in range(last_line_fill,line):
+#                speed[blank_line] = current_speed
+#            last_line_fill = line
+#        #at the end of reward region, when the distance measurement resets to zero
+#        elif(s2 < s1 and s1-s2 > 50.00):
+#            t2 = raw_behavior[line-1][1]
+#            s2 = raw_behavior[line-1][7]          
+#            current_speed = ((s2 - s1)*1000.00)/(t2 - t1) #speed is in mm/s
+#            for blank_line in range(last_line_fill,line):
+#                speed[blank_line] = current_speed
+#            last_line_fill = line            
+#            t1 = raw_behavior[line][1]
+#            s1 = raw_behavior[line][7]
+#        #at the end of the recording
+#        elif(line == len(raw_behavior)-1):
+#            t2 = raw_behavior[line][1]
+#            s2 = raw_behavior[line][7]          
+#            current_speed = ((s2 - s1)*1000.00)/(t2 - t1) #speed is in mm/s
+#            for blank_line in range(last_line_fill,line+1):
+#                speed[blank_line] = current_speed
+#            last_line_fill = line            
+
+
+    #calculate speed per 500ms of the recording time########################################
+    s1 = 0.00
+    s2 = 0.00
+    t1 = 0.00
+    t2 = 0.00
+    last_line_fill = 0
+    speed = numpy.zeros(len(raw_behavior),dtype='int')
+
+    
+    current_speed = 0 
+    
+    for line in range(0,len(raw_behavior)):
+        #read current distance and time
+        t2 = raw_behavior[line][1]
+        s2 = raw_behavior[line][7]
+        #sanity_check = raw_behavior[line-1][6]
+        #if there is no change in time, the speed remains unchanged
+        #speed is calculated for every 500 ms window in this case
+        if (t2 - t1 > 500 and abs(s2-s1) < 2000):
+            t2 = raw_behavior[line-1][1]
+            s2 = raw_behavior[line-1][7]
+            current_speed = int(((s2 - s1)*1000.00)/(t2 - t1)) #speed is in mm/s
+
+            t1 = raw_behavior[line][1]
+            s1 = raw_behavior[line][7]
+            
+            for blank_line in range(last_line_fill,line):
+                speed[blank_line] = current_speed
+            last_line_fill = line
+        
+        #at the end of reward region, when the distance measurement resets to zero
+        elif(abs(s2-s1) > 2000 and t2 - t1 >= 100): #to reset values when a new track starts
+            t2 = raw_behavior[line-1][1]
+            s2 = raw_behavior[line-1][7]          
+            current_speed = int(((s2 - s1)*1000.00)/(t2 - t1)) #speed is in mm/s
+            for blank_line in range(last_line_fill,line):
+                speed[blank_line] = current_speed
+            last_line_fill = line            
+            t1 = raw_behavior[line][1]
+            s1 = raw_behavior[line][7]
+        elif(abs(s2-s1) > 2000 and t2 - t1 < 100): #to reset values when a new track starts         
+            current_speed = speed[last_line_fill] #speed is in mm/s
+            for blank_line in range(last_line_fill+1,line):
+                speed[blank_line] = current_speed
+            last_line_fill = line            
+            t1 = raw_behavior[line][1]
+            s1 = raw_behavior[line][7]
+
+        #at the end of the recording
+        elif(line == len(raw_behavior)-1 and t2-t1 > 0):
+            current_speed = int(((s2 - s1)*1000.00)/(t2 - t1)) #speed is in mm/s
+            for blank_line in range(last_line_fill,line+1):
+                speed[blank_line] = current_speed
+            
+    min_speed = min(speed)
+    max_speed = max(speed)
+    print 'Minimum speed was: %f' %min_speed 
+    print 'Maximum speed was: %f' %max_speed 
+    
+    zero_speed_times = []
+
+    for speed_row in range(0,len(speed)):
+        if(speed[speed_row] == min_speed or speed[speed_row] == max_speed):     
+            if(len(zero_speed_times) == 0):
+                zero_speed_times = [raw_behavior[speed_row][1],speed[speed_row]]
+            else:
+                zero_speed_times = numpy.vstack([zero_speed_times,[raw_behavior[speed_row][1],speed[speed_row]]]) 
+
+    numpy.savetxt(raw_behavior_file.replace('.csv','_time_points_for_min_max_speeds.csv'), zero_speed_times, fmt='%.2f', delimiter=',', newline='\n')
+
+    #numpy.savetxt(raw_behavior_file.replace('.csv','_speeeeeeeeeeeeeeeed.csv'), speed, fmt='%f', delimiter=',', newline='\n')
+
+    #done calculating speed per 50mm stretch of the track########################################
+    #############################################################################################
+
+
+
+
+    #use and save only the relevant columns from the raw behavior file
+    behavior = numpy.empty((raw_behavior[len(raw_behavior) - 1][0],9),dtype='int')
+    frame_count = 0
+    
+    for row in range(0, len(raw_behavior)):
+        #save data only if there's a new frame in the current data reading
+        if(raw_behavior[row][0] > frame_count):
+            behavior[frame_count][0] = raw_behavior[row][0]    #frame count
+            behavior[frame_count][1] = raw_behavior[row][1]    #time
+            behavior[frame_count][2] = raw_behavior[row][2]    #odor
+            behavior[frame_count][3] = raw_behavior[row][3]    #lick count
+            behavior[frame_count][4] = raw_behavior[row][4]    #reward count
+#            behavior[frame_count][4] = raw_behavior[row][5]    #initial drop count
+#            behavior[frame_count][4] = raw_behavior[row][6]    #reward window status
+            behavior[frame_count][5] = raw_behavior[row][7]    #distance
+            behavior[frame_count][6] = raw_behavior[row][9]    #lap count
+            behavior[frame_count][7] = raw_behavior[row][10]   #lap count
+            behavior[frame_count][8] = speed[row]              #speed
+            frame_count = raw_behavior[row][0] 
+    
+    print "Behavior array size is: "
+    print behavior.shape
+    number_of_frames = behavior.shape[0]
+    print 'Total number of frames recorded by arduino: %d'%number_of_frames
+
+    
+###############################################################################
+
+
+    valid_cells = numpy.loadtxt(valid_cells_file, dtype='int', comments='#', delimiter=',')
+    print "Number of putative cells: %d"%valid_cells.size
+    number_of_valid_cells = numpy.sum(valid_cells) 
+    print "Number of valid cells in this recording: %d"%number_of_valid_cells   
+###############################################################################
+
+    
+    #Source: http://stackoverflow.com/questions/4974290/reading-non-uniform-data-from-file-into-array-with-numpy
+    f = open(events_file, "r")
+    events = []
+    line = f.readline()
+    index = 0
+    while line:
+        line = line.strip("\n")
+        line = line.split()
+        events.append([])
+        for item in line:
+            events[index].append(int(item))
+        line = f.readline()
+        index += 1
+    f.close()
+    print 'Size of events file is: %d'%len(events) 
+    print 'Some events for cells(valid as well as impostors):'
+    print events[0:5]
+###############################################################################
+
+
+    #create an array of array with only valid cells
+    valid_cell_events = []
+    for cell in range(0,len(events)):
+        if(valid_cells[cell] == 1):
+            valid_cell_events.append(events[cell])
+    print 'Size of valid cell events file is: %d'%len(valid_cell_events)    
+    print valid_cell_events[0:3]    
+    #print valid_cell_events[len(valid_cell_events)-6:len(valid_cell_events)]
+    
+    #    
+    events_by_frame = numpy.empty((number_of_frames,number_of_valid_cells),dtype='int')   
+    max_frame_with_event = 0
+    for this_cell in range(0,number_of_valid_cells):
+        frames_in_this_cell = len(valid_cell_events[this_cell])
+        for this_frame in range(0,frames_in_this_cell):
+            event_frame_index = (valid_cell_events[this_cell][this_frame] -1)*2 #multiply by 2 b/c imaging data has been down sampled (10 frames/sec to 5)
+            events_by_frame[event_frame_index][this_cell] = 1
+            if(event_frame_index > max_frame_with_event):
+                max_frame_with_event = event_frame_index
+    print 'The last event was registered at frame# %d'%max_frame_with_event
+###############################################################################    
+
+
+    # run this only if there are missing frames
+    missing_frames = []
+    if (missing_frame_file == 0):
+        missing_frames = [0]
+        print 'There are no dropped frames in this recording :)'
+    else:
+        missing_frames = numpy.loadtxt(missing_frame_file, dtype='int', comments='#', delimiter=',')
+        print "Number of dropped frames: %d"%missing_frames.size
+###############################################################################
+
+    print 'Now we will nicely pack and save all this information in one csv file:'
+    events_adjusted_for_missing_frames = numpy.empty((number_of_frames,behavior.shape[1] + number_of_valid_cells),dtype='int')
+
+    adjust_for_missing_frames = 0
+    
+    for frame in range(0,number_of_frames):
+        if(frame+1 in missing_frames):
+            adjust_for_missing_frames = adjust_for_missing_frames + 1
+            for cell in range(0,events_adjusted_for_missing_frames.shape[1]):
+                if (cell < behavior.shape[1]):
+                    events_adjusted_for_missing_frames[frame][cell] = behavior[frame][cell]
+                else:
+                    events_adjusted_for_missing_frames[frame][cell] = 0 # use numpy.nan to be more exact, b/c the frame is missing
+        #business is as usual if frames are not missing:
+        else:
+            for cell in range(0,events_adjusted_for_missing_frames.shape[1]):
+                if (cell < behavior.shape[1]):
+                    events_adjusted_for_missing_frames[frame][cell] = behavior[frame][cell]
+                else:
+                    events_adjusted_for_missing_frames[frame][cell] = events_by_frame[frame - adjust_for_missing_frames][cell-behavior.shape[1]]           
+    
+    print 'The saved array size is:'
+    print events_adjusted_for_missing_frames.shape
+    #now save the array as a csv file in the same location as the input file
+    #numpy.savetxt(raw_behavior_file.replace('.csv','_and_events.csv'), events_adjusted_for_missing_frames, fmt='%1.3f', delimiter=',', newline='\n')
+    numpy.savetxt(raw_behavior_file.replace('.csv','_and_events.csv'), events_adjusted_for_missing_frames, fmt='%i', delimiter=',', newline='\n')
+###############################################################################
+###############################################################################
+###############################################################################
+
+#we need the files to be in the following order for the analysis to run properly:
+#1 behavior
+#2 events
+#3 valid_cells
+#4 missing frames
+
+#to make sure that the files are processed in the proper order (not really important here, but just in case)
+def natural_key(string_):
+    """See http://www.codinghorror.com/blog/archives/001018.html"""
+    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+
+
+def main():
+    #detect all the .csv files in the folder
+    file_names = [os.path.join(directory_path, f)
+        for dirpath, dirnames, files in os.walk(directory_path)
+        for f in files if f.endswith('.csv')]
+    file_names.sort(key=natural_key)
+
+    #now on to the actual analysis:
+    if (len(file_names) == 3):
+        print 'Looks like no imaging frames were dropped in this recording. Great!'
+        print 'Events file: '+ file_names[0]
+        print 'Valid cells: '+ file_names[1]
+        print 'Behavior file: '+ file_names[2]
+        extract_details_per_frame(file_names[0],file_names[1],file_names[2],0)
+    # if there are missing frames, run this statement
+    elif (len(file_names) == 4):
+        print 'Some imaging frames are missing in this recording.'
+        print 'Events file: '+ file_names[0]
+        print 'Valid cells: '+ file_names[1]
+        print 'Behavior file: '+ file_names[2]
+        print 'Missing frame file: '+ file_names[3]
+        extract_details_per_frame(file_names[0],file_names[1],file_names[2],file_names[3])
+    else:
+        print "Please make sure that there are an appropriate number of files in the folder"
+
+main()
+
